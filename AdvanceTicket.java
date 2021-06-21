c81243c185125e35f4e9a215c81bad8e49eb5913
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-06-20 21:54:01 -0900

    The class for AdvanceTicket is added and complete

--- AdvanceTicket.java
+++ AdvanceTicket.java
@@ -1,3 +1,7 @@
+/*The AdvanceTicket object has price, serial number and number of days to event.
+It determines the price according to days left to event.
+This class is a subclass of abstract class Tickets
+ */
 package CampusTickets;
 
 public class AdvanceTicket extends Tickets {
@@ -5,5 +9,30 @@ public class AdvanceTicket extends Tickets {
     private double price;
     private int numOfDaysToEvent; //stores the number of days left before event starts
 
-    //
+    //Constructor that takes serial number and days left to event as parameters
+    public AdvanceTicket(String serialNumber, int numOfDaysToEvent){
+        //calls the parent constructor
+        super(serialNumber);
+        this.numOfDaysToEvent = numOfDaysToEvent;
+    }
+
+    //return the number of days in advance ticket was purchased
+    public int getDays(){
+
+        return numOfDaysToEvent;
+    }
+
+    //Implements the abstract method getPrice
+    public double getPrice(){
+
+        /*If the ticket is purchased 10 days ot more in advance
+        set price to R30.00 else R40.00
+         */
+        if(numOfDaysToEvent >= 10){
+
+            price = 30.00;
+        }else{
+            price = 40.00;
+        }
+    }
 }

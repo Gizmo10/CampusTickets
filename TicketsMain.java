3799ad5ecaac3ec234fc2db165106e3c534f30d4
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-06-21 12:23:59 -0900

    The main class is used to test the subclass methods and find bugs in the program

---
+++ TicketsMain.java
@@ -0,0 +1,13 @@
+/*This class is the main class for the Ticket subclasses and
+it is used to test the methods of the subclasses. The client code
+is used to find nugs in the subclasses.
+ */
+
+package CampusTickets;
+
+public class TicketsMain {
+    //The main method
+    public static void main(String [] args){
+
+    }
+}

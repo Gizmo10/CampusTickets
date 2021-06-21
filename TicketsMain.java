a42f0713a06ff863f46361fb675d3c64a9037066
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-06-21 12:32:55 -0900

    array of Ticket objects created

--- TicketsMain.java
+++ TicketsMain.java
@@ -1,6 +1,6 @@
 /*This class is the main class for the Ticket subclasses and
 it is used to test the methods of the subclasses. The client code
-is used to find nugs in the subclasses.
+is used to find bugs in the subclasses.
  */
 
 package CampusTickets;
@@ -8,6 +8,13 @@ package CampusTickets;
 public class TicketsMain {
     //The main method
     public static void main(String [] args){
+        /*Declares an array of Ticket objects and the parent
+        abstract class points to it's subclasses using polymorphic
+        assignment.
+         */
+        Tickets [] eventTickets = {new WalkupTicket("1001"), new AdvanceTicket("1002",11),
+        new AdvanceTicket("1003", 7),new StudentAdvanceTicket("1004",13),
+        new StudentAdvanceTicket("1005",5)};
 
     }
 }
